188b1f12eb90e699c7bd96d3f076d018dd094795
Jean Izepon <izeponjc@gmail.com>
2022-05-04 15:22:03 -0300

    gitignore

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users/', (req, res) => {
-    return res.send('Hello World');
-});
-
 app.listen(3333, () => {
     console.log('HTTP server running!');
 });
