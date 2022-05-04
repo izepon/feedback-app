f9de59cde7b6b0518f8db13bed12c024c7208af9
Jean Izepon <izeponjc@gmail.com>
2022-05-04 15:15:41 -0300

    install express and prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/users/', (req, res) => {
+    return res.send('Hello World');
+});
+
+app.listen(3333, () => {
+    console.log('HTTP server running!');
+});
